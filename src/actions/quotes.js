2e91baff5e3bfc4fb3fa44b2e90c0d963ad2735c
Kenneth Spatta <kenyounot@gmail.com>
2020-02-02 17:37:31 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,34 @@
 // TODO: Create action creators as defined in tests
+
+export const addQuote = quote => {
+	return {
+		quote: {
+			id: quote.id,
+			content: quote.content,
+			author: quote.author,
+			votes: 0
+		},
+		type: 'ADD_QUOTE'
+	};
+};
+
+export const removeQuote = quoteID => {
+	return {
+		type: 'REMOVE_QUOTE',
+		quoteId: quoteID
+	};
+};
+
+export const upvoteQuote = quoteID => {
+	return {
+		type: 'UPVOTE_QUOTE',
+		quoteId: quoteID
+	};
+};
+
+export const downvoteQuote = quoteID => {
+	return {
+		type: 'DOWNVOTE_QUOTE',
+		quoteId: quoteID
+	};
+};
